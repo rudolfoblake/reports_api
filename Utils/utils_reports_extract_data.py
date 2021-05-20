b65a4ce6804fd787986d299120a837c77ac705ac
ORLANDO CECHLAR BITENCOURT <orlando.cechlar@gmail.com>
2021-05-20 14:16:58 -0300

    Update utils_reports_extract_data.py

--- Utils/utils_reports_extract_data.py
+++ Utils/utils_reports_extract_data.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 
 
@@ -15,8 +13,3 @@ def get_data_from_order_logs(url: str, filter_data: dict) -> dict:
         return_msg = Error
 
     return dict(status=return_status, message=return_msg, data=data['orders'])
-
-
-# dates = {"initial_date": '2021-05-10', "final_date": '2021-05-20'}
-# a = get_data_from_order_logs(url="http://127.0.0.1:8000/orders/reports/2", filter_data=dates)
-# print(a['data'])
